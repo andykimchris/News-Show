fef4c0c56e7fad9a8ffefd0c06fd541fe9c383b9
Andy <andrew.muchiri97@gmail.com>
2018-09-03 13:41:27 +0300

    sets up user and news letter model

--- news_show/news/models.py
+++ news_show/news/models.py
@@ -2,5 +2,23 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=False, blank=False, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    email_address = models.EmailField()
+
+
+    def __str__(self):
+        return self.name
+
+def create_user(sender, **kwargs):
+    if kwargs['created']:
+        user_profile=UserProfile.objects.create(user=kwargs['instance'])
+
+class NewsLetterRecipients(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField()
