abe7542dcd8ada150ec4363f08c201e4ad8c98a4
Andy <andrew.muchiri97@gmail.com>
2018-09-03 13:49:57 +0300

    adds sources and articles urls

--- news_show/news/urls.py
+++ news_show/news/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('sources/', views.sources, name="sources"),
+    path('articles/', views.articles, name="articles")
 ]
