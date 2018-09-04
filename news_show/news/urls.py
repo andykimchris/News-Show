c2a5fe1e6f27836043ce0d990a4b685690646f73
Andy <andrew.muchiri97@gmail.com>
2018-09-04 10:50:53 +0300

    project restructure

--- news_show/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('sources/', views.sources, name="sources"),
-    path('articles/', views.articles, name="articles")
-]
