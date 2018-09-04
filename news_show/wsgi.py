8b94743005ace102737cc655e89a192bb6b10dde
Andy <andrew.muchiri97@gmail.com>
2018-09-04 10:31:55 +0300

    serves the app

---
+++ news_show/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for news_show project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+
+import os
+from whitenoise.django import DjangoWhiteNoise
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news_show.settings')
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
